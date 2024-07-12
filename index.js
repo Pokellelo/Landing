f6445a6630daa617396697f58f817937be74ab3c
Pokellelo <pokellelo@gmail.com>
2024-07-11 21:42:33 -0600

    basic usability of draggin elements

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const defaultElement = {
   is_url: false,
 };
 
-const main = document.getElementById("main")
+const main = document.getElementById("main");
 
 const regex_text =
   /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
@@ -21,11 +21,11 @@ let selected_id = null;
 let being_paste = false;
 
 const generateElement = (e) => {
-  console.log(e);
-
-
-
-
+  const d = document.createElement("div");
+  d.className = "basic-element";
+  const text = document.createTextNode("Hi there and greetings!");
+  d.appendChild(text);
+  main.appendChild(d);
 };
 
 const removeElement = (index) => {
@@ -84,3 +84,47 @@ var elements = e ? e : [];
 generateAllElements();
 
 console.log(elements);
+
+let divs = document.getElementsByClassName("basic-element");
+for (div of divs) div.onmousedown = onMouseDown;
+
+document.onmousemove = onMouseMove;
+document.onmouseup = onMouseUp;
+
+var the_moving_div = "";
+var the_last_mouse_position = { x: 0, y: 0 };
+
+function onMouseDown(e) {
+  e.preventDefault();
+  the_moving_div = e.target.id; // remember which div has been selected
+  the_last_mouse_position.x = e.clientX; // remember where the mouse was when it was clicked
+  the_last_mouse_position.y = e.clientY;
+  e.target.style.border = "2px solid blue"; // highlight the border of the div
+
+  var divs = document.getElementsByTagName("div");
+  e.target.style.zIndex = divs.length; // put this div  on top
+  var i = 1;
+  for (div of divs) if (div.id != the_moving_div) div.style.zIndex = i++; // put all other divs behind the selected one
+}
+
+function onMouseMove(e) {
+  e.preventDefault();
+
+  if (the_moving_div == "") return;
+
+  var d = document.getElementById(the_moving_div);
+  d.style.left = d.offsetLeft + e.clientX - the_last_mouse_position.x + "px"; // move the div by however much the mouse moved
+  d.style.top = d.offsetTop + e.clientY - the_last_mouse_position.y + "px";
+  the_last_mouse_position.x = e.clientX; // remember where the mouse is now
+  the_last_mouse_position.y = e.clientY;
+  onMove();
+}
+
+function onMouseUp(e) {
+  e.preventDefault();
+  if (the_moving_div == "") return;
+  document.getElementById(the_moving_div).style.border = "none"; // hide the border again
+  the_moving_div = "";
+}
+
+function onMove() {}
