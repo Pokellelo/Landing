b4c9cd977a8201ca7ecae33eaf6187494cf5e7a1
Pokellelo <pokellelo@gmail.com>
2024-07-14 21:08:18 -0600

    moving some code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Variables globales
 const defaultElement = {
   id: 0,
   index: null,
@@ -19,15 +20,33 @@ const main = document.getElementById("main");
 
 const regex_url =
   /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
+const the_last_mouse_position = { x: 0, y: 0 };
+
 let selected_id = null;
 let being_paste = false;
 let resized = null;
+let unique_tags = {};
+var the_moving_div = "";
+let mousemoving = false;
+
+/*********Functions*********/
+
+//General Use - not binded to anything
+const setStorage = (id, value) => {
+  localStorage.setItem(id, JSON.stringify(value));
+};
+
+const getStorage = (id) => {
+  const data = localStorage.getItem(id);
+  return data ? JSON.parse(data) : undefined;
+};
 const reset = () => {
   localStorage.clear();
   location.reload();
 };
-
 const resizeObserver = new ResizeObserver((entries) => {
+  //Create an resize observer to an element
   resized = {
     id: entries[0].target.childNodes[0].id,
     height: entries[0].contentRect.height,
@@ -35,11 +54,22 @@ const resizeObserver = new ResizeObserver((entries) => {
   };
 });
 
+const parentbyid = (id) => {
+  return document.querySelector("#" + id).parentElement;
+};
+
+const createElement_withAtributes = (attributes = {}, tag = "div") => {
+  const e = document.createElement(tag);
+  for (let key in attributes) {
+    e[key] = attributes[key];
+  }
+  return e;
+};
+
+//Functions binded to some feature
+
 const generateElement = (e, index) => {
   let type_tag = "div";
-
-  let unique_tags = {};
-
   if (e.is_text) {
     type_tag = "textarea";
     unique_tags.title = "text" + e.id;
@@ -52,8 +82,11 @@ const generateElement = (e, index) => {
     unique_tags.target = "_";
   } else if (e.is_canvas) type_tag = "iframe";
 
-  const d = document.createElement(type_tag);
-  const wrapper = document.createElement("div");
+  const d = createElement_withAtributes(unique_tags, type_tag);
+
+  const wrapper = createElement_withAtributes("div");
+  const toolbar = createElement_withAtributes("div");
+  toolbar.className = "element-toolbar";
 
   d.id = "ele" + e.id;
   wrapper.style.color = e.text_color;
@@ -67,7 +100,7 @@ const generateElement = (e, index) => {
 
   wrapper.className = "wrapper";
 
-  d.className = "basic-element";
+  d.className = "element";
   d.onmousedown = onMouseDown;
   const text = document.createTextNode(e.inner_value);
 
@@ -78,6 +111,7 @@ const generateElement = (e, index) => {
   }
 
   wrapper.appendChild(d);
+  wrapper.appendChild(toolbar);
 
   main.appendChild(wrapper);
 
@@ -114,16 +148,7 @@ const setElement = (type, value = "") => {
   generateElement(newElement, index);
 };
 
-//localStorage.clear()
-const setStorage = (id, value) => {
-  localStorage.setItem(id, JSON.stringify(value));
-};
-
-const getStorage = (id) => {
-  const data = localStorage.getItem(id);
-  return data ? JSON.parse(data) : undefined;
-};
-
+//Handlers, listeners
 document.onpaste = async (evt) => {
   if (being_paste) return;
   being_paste = true;
@@ -156,40 +181,7 @@ document.onpaste = async (evt) => {
   }
 };
 
-//Init
-
-const e = getStorage("elements");
-const iiu = getStorage("ids_in_use");
-const bc = getStorage("background_color");
-
-let elements = e ? e : [];
-const ids_in_use = iiu ? iiu : [0];
-let background_color = bc ? bc : "white";
-
-generateAllElements();
-
-document.onmousemove = onMouseMove;
-document.onmouseup = onMouseUp;
-
-var the_moving_div = "";
-var the_last_mouse_position = { x: 0, y: 0 };
-let mousemoving = false;
-
-function onMouseDown(e) {
-  mousemoving = true;
-  e.preventDefault();
-  the_moving_div = e.target.id; // remember which div has been selected
-  the_last_mouse_position.x = e.clientX; // remember where the mouse was when it was clicked
-  the_last_mouse_position.y = e.clientY;
-  e.target.parentElement.style.border = "2px solid blue"; // highlight the border of the div
-
-  var divs = document.getElementsByClassName("wrapper");
-  e.target.style.zIndex = divs.length; // put this div  on top
-  var i = 1;
-  for (div of divs) if (div.id != the_moving_div) div.style.zIndex = i++; // put all other divs behind the selected one
-}
-
-function onMouseMove(e) {
+document.onmousemove = (e) => {
   e.preventDefault();
   if (the_moving_div == "") return;
 
@@ -199,9 +191,9 @@ function onMouseMove(e) {
   d.style.top = d.offsetTop + e.clientY - the_last_mouse_position.y + "px";
   the_last_mouse_position.x = e.clientX; // remember where the mouse is now
   the_last_mouse_position.y = e.clientY;
-}
+};
 
-function onMouseUp(e) {
+document.onmouseup = (e) => {
   if (resized) {
     const rs = document.getElementById(resized.id);
     elements[rs.id.slice(-1) - 1].height = resized.height + "px";
@@ -224,4 +216,30 @@ function onMouseUp(e) {
   elements[d.id.slice(-1) - 1].positionX = d.parentElement.style.left;
 
   setStorage("elements", elements);
-}
+};
+
+const onMouseDown = (e) => {
+  mousemoving = true;
+  e.preventDefault();
+  the_moving_div = e.target.id; // remember which div has been selected
+  the_last_mouse_position.x = e.clientX; // remember where the mouse was when it was clicked
+  the_last_mouse_position.y = e.clientY;
+  e.target.parentElement.style.border = "2px solid blue"; // highlight the border of the div
+
+  var divs = document.getElementsByClassName("wrapper");
+  e.target.style.zIndex = divs.length; // put this div  on top
+  var i = 1;
+  for (div of divs) if (div.id != the_moving_div) div.style.zIndex = i++; // put all other divs behind the selected one
+};
+
+//Initialization
+
+const e = getStorage("elements");
+const iiu = getStorage("ids_in_use");
+const bc = getStorage("background_color");
+
+let elements = e ? e : [];
+const ids_in_use = iiu ? iiu : [0];
+let background_color = bc ? bc : "white";
+
+generateAllElements();
